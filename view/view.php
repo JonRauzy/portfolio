6823f8206d7b1d2618eb37e045e2998df8e034cd
JonRauzy <jonathan.rauzy@gmail.com>
2023-02-20 15:23:08 +0100

    2lines

--- view/view.php
+++ view/view.php
@@ -1,8 +1,10 @@
 <?php
+// l'affichage final de tout :
 include_once 'include/header.php';
 ?>
 
 <body>
     <h1>Portfolio</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis totam, dicta quasi vero nulla non deserunt expedita quaerat. Commodi eligendi culpa autem accusamus molestiae dignissimos nulla temporibus inventore assumenda quae?</p>
 </body>
 </html>
