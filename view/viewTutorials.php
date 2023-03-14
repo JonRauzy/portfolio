782a39a77556df8d1e484b922742f37e40f945c2
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-14 15:20:47 +0100

    c'est mieux

--- view/viewTutorials.php
+++ view/viewTutorials.php
@@ -4,8 +4,24 @@ $titre = 'tutoriels';
 require_once '../view/include/header.php';
 ?>
 
-
-
+<h1>tutoriels</h1>
+<div class="container-tuto">
+    <div class="video">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur esse, odit nobis recusandae accusamus nemo est. Maiores expedita porro officiis praesentium. Aspernatur ipsa mollitia voluptate officia. Minima consectetur iste suscipit?</p>
+        <iframe width="900" height="800" src="https://www.youtube.com/embed/8mAITcNt710" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>
+    <div class="tuto-text">
+        <h2>Comment faire une fonction :</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit magni similique obcaecati porro tempore non sed minima, velit vero saepe ex laboriosam laudantium ipsa aperiam, asperiores corporis maxime quasi ipsam?</p>
+        <h3>On passe aux choses sérieuse :</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, praesentium cupiditate. Aperiam est porro iure accusamus cumque amet numquam placeat earum voluptatibus delectus, error aspernatur explicabo facere rerum. Sapiente, culpa.</p>
+        <h3>Et finalement :</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam tempora modi nulla, maiores magni voluptate rerum quis obcaecati labore odio eos, fuga error ex suscipit iste perferendis adipisci. Dolor, quia!</p>
+    </div>
+</div>
 
 <?php
 include_once '../view/include/footer.php';
