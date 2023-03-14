f2d9df97998b3a705a6574c46c884443397386c1
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-14 08:39:26 +0100

    amélioration mise en page

--- view/viewProjectID.php
+++ view/viewProjectID.php
@@ -1,22 +1,23 @@
 <?php
 
-foreach ($projectSolo as $value):
+foreach ($projectSolo as $value) :
     $titre = $value['title'];
     include_once 'include/header.php';
 ?>
 
-
-    <div class="project-id">
+    <div class="container-project-id">
         <h1><?= $value['title'] ?></h1>
-        <p><?= $value['description'] ?></p>
-        <a href="<?= $value['link'] ?>">Cliquez ici pour visiter le site</a>
-        <img src="<?= $value['img'] ?>" alt="">
-<?php
-endforeach;
-?>
-    <a href="?p=projects" style="font-size: 1.2rem">retour</a>
-</div>
+        <div class="project-id">
+            <p><?= $value['description'] ?></p>
+            <a href="<?= $value['link'] ?>">Cliquez ici pour visiter le site</a>
+            <img src="<?= $value['img'] ?>" alt="">
+        <?php
+    endforeach;
+        ?>
+        <a href="?p=projects" style="font-size: 1.2rem">retour</a>
+        </div>
+    </div>
 
 
-<?php
-include_once '../view/include/footer.php';
+    <?php
+    include_once '../view/include/footer.php';
