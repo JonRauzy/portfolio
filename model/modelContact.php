aafe3d5c2135091c47cd5e688ecd2e429e6ffb44
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-15 10:01:09 +0100

    tuto 2.0

--- model/modelContact.php
+++ model/modelContact.php
@@ -1,5 +1,37 @@
 <?php
 
+// use Symfony\Component\Mailer\Transport;
+// use Symfony\Component\Mailer\Mailer;
+// use Symfony\Component\Mime\Email;
+
+
+// function sendMail()
+// {
+    
+//     $name = htmlspecialchars(strip_tags(trim($_POST['name'])), ENT_QUOTES);
+//     $email = htmlspecialchars(strip_tags(trim($_POST['email'])));
+//     $message = htmlspecialchars(strip_tags(trim($_POST['message'])), ENT_QUOTES);
+//     $object = $_POST['object'];
+    
+//     $transport = Transport::fromDsn(MAIL_DSN);
+//     $mailer = new Mailer($transport);
+
+//     $email = (new Email())
+//         ->from(@$email)
+//         ->to(MAIL_ADMIN)
+//         //->cc('cc@example.com')
+//         //->bcc('bcc@example.com')
+//         //->replyTo('fabien@example.com')
+//         //->priority(Email::PRIORITY_HIGH)
+//         ->subject($name . "a dit " . $object)
+//         ->text($message)
+//         ->html("<p>$message</p>");
+
+//     $mailer->send($email);
+// }
+
+
+
 function insertMail($theDB)
 {
     $name = htmlspecialchars(strip_tags(trim($_POST['name'])), ENT_QUOTES);
@@ -17,33 +49,3 @@ function insertMail($theDB)
         return "y'a des problème!";
     }
 }
-
-
-// use Symfony\Component\Mailer\Transport;
-// use Symfony\Component\Mailer\Mailer;
-// use Symfony\Component\Mime\Email;
-
-// function sendMail()
-// {
-//     $name = htmlspecialchars(strip_tags(trim($_POST['name'])), ENT_QUOTES);
-//     $email = htmlspecialchars(strip_tags(trim($_POST['email'])));
-//     $message = htmlspecialchars(strip_tags(trim($_POST['message'])), ENT_QUOTES);
-//     $object = $_POST['object'];
-
-
-//     $transport = Transport::fromDsn(MAIL_DSN);
-//     $mailer = new Mailer($transport);
-
-//     $email = (new Email())
-//         ->from(@$email)
-//         ->to(MAIL_ADMIN)
-//         //->cc('cc@example.com')
-//         //->bcc('bcc@example.com')
-//         //->replyTo('fabien@example.com')
-//         //->priority(Email::PRIORITY_HIGH)
-//         ->subject($name . "a dit " . $object)
-//         ->text($message)
-//         ->html("<p>$message</p>");
-
-//     $mailer->send($email);
-// }
--- view/viewTutorials.php
+++ view/viewTutorials.php
@@ -6,20 +6,39 @@ require_once '../view/include/header.php';
 
 <h1>tutoriels</h1>
 <div class="container-tuto">
-    <div class="video">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur esse, odit nobis recusandae accusamus nemo est. Maiores expedita porro officiis praesentium. Aspernatur ipsa mollitia voluptate officia. Minima consectetur iste suscipit?</p>
+    <div class="tuto-select">
+        <div class="tuto-card-1">
+            <h4>Tutoriel 1 : Intro à la programmation par Harvard </h4>
+            <a href="#tuto-1"><img src="img/tuto1" alt=""></a>
+        </div>
+        <div class="tuto-card-2">
+            <h4>Tutoriel 2 : Comment faire une fonction en JavaScript</h4>
+            <a href="#tuto-2"><img src="img/tuto2" alt=""></a>
+        </div>
+    </div>
+    <div class="video" id="tuto-1">
+        <h2>Introdution à la programmation : </h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur esse, odit nobis recusandae accusamus
+            nemo est. Maiores expedita porro officiis praesentium. Aspernatur ipsa mollitia voluptate officia. Minima
+            consectetur iste suscipit?</p>
         <iframe width="900" height="800" src="https://www.youtube.com/embed/8mAITcNt710" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
     </div>
-    <div class="tuto-text">
+    <div class="tuto-text" id="tuto-2">
+
         <h2>Comment faire une fonction :</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit magni similique obcaecati porro tempore non sed minima, velit vero saepe ex laboriosam laudantium ipsa aperiam, asperiores corporis maxime quasi ipsam?</p>
-        <h3>On passe aux choses sérieuse :</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, praesentium cupiditate. Aperiam est porro iure accusamus cumque amet numquam placeat earum voluptatibus delectus, error aspernatur explicabo facere rerum. Sapiente, culpa.</p>
-        <h3>Et finalement :</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam tempora modi nulla, maiores magni voluptate rerum quis obcaecati labore odio eos, fuga error ex suscipit iste perferendis adipisci. Dolor, quia!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit magni similique obcaecati porro tempore non
+            sed minima, velit vero saepe ex laboriosam laudantium ipsa aperiam, asperiores corporis maxime quasi ipsam?
+        </p>
+        <h4>On passe aux choses sérieuse :</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, praesentium cupiditate. Aperiam est
+            porro iure accusamus cumque amet numquam placeat earum voluptatibus delectus, error aspernatur explicabo
+            facere rerum. Sapiente, culpa.</p>
+        <h4>Et finalement :</h4>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam tempora modi nulla, maiores magni voluptate
+            rerum quis obcaecati labore odio eos, fuga error ex suscipit iste perferendis adipisci. Dolor, quia!</p>
     </div>
 </div>
 
