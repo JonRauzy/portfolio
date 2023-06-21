25a068f57488f3f478ce8056e6813cd9d33b7849
JonRauzy <jonathan.rauzy@gmail.com>
2023-06-21 15:06:06 +0200

    gros changement

--- view/viewPrivate.php
+++ view/viewPrivate.php
@@ -5,7 +5,8 @@ require_once '../view/include/header.php'
 
 <div class="connected">
 <h1>Salut on te connait</h1>
-<a href="?p=homepage" style="text-align: center;"><p>&#x2190; Retour à l'accueil<p></a>
+<a href="./" style="text-align: center;"><p>&#x2190; Retour à l'accueil<p></a>
 </div>
+
 <?php
 include_once '../view/include/footer.php';
